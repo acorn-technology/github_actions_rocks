26d1e21913a11bc5daa2a68a53954706ac6b875c
Chris Fogelklou <chris.fogelklou@gmail.com>
2021-04-09 16:44:51 +0200

    'Adding scripts to commit everything.'

--- checkout_branch.sh
+++ checkout_branch.sh
@@ -9,7 +9,7 @@ pushd acornSmartWatchTuner && git checkout -B $1 && popd
 pushd battlesnake-2017 && git checkout -B $1 && popd
 pushd cv_generator_2018 && git checkout -B $1 && popd
 pushd gamercorn && git checkout -B $1 && popd
-pushd hemligt_project53 && git checkout -B $1 && popd
+pushd hemligt_projekt53 && git checkout -B $1 && popd
 pushd sellist && git checkout -B $1 && popd
 pushd snow && git checkout -B $1 && popd
 
