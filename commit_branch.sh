b3757c8cb304302e3e0cf45f667ae7e768ec9bc4
Chris Fogelklou <chris.fogelklou@gmail.com>
2021-04-15 15:41:46 +0200

    'Update with latest script'

--- commit_branch.sh
+++ commit_branch.sh
@@ -8,12 +8,10 @@ str="'$*'"
 #echo "$str"
 
 pushd acornSmartWatchTuner || git commit . -m "$str" || popd || git stage acornSmartWatchTuner
-pushd battlesnake-2017 || git commit . -m "$str" || popd || git stage battlesnake-2017
-pushd cv_generator_2018 || git commit . -m "$str" || popd || git stage cv_generator_2018 || 
-pushd gamercorn || git commit . -m "$str" || popd || git stage gamercorn 
-pushd hemligt_projekt53 || git commit . -m "$str" || popd || git stage hemligt_projekt53
-pushd sellist || git commit . -m "$str" || popd || git stage sellist
-pushd onboarding || git commit . -m "$str" || popd || git stage onboarding
+pushd BattleSnake-cpp-client || git commit . -m "$str" || popd || git stage BattleSnake-cpp-client
+pushd roughtime_cpp_client || git commit . -m "$str" || popd || git stage roughtime_cpp_client
+pushd hello-electron || git commit . -m "$str" || popd || git stage hello-electron
+
 
 git commit . -m "$str"
 
--- shortcuts.md
+++ shortcuts.md
@@ -5,3 +5,6 @@ Plugins used: slides, presentation-mode, presentation-buddy
 Open slides mode: ⌘ ⌥ P (or on windows keyboard, ⌘ alt P)
 Open command window: ⌘ shift P then presentation-buddy: start/continue
 Next slide: ⌘ alt =>
+
+Convert YAML-->JSON: Must be on .yml file, cmd+shift+p, type json
+Convert JSON-->YAML: Must be on json file, cmd+shift+p, type yaml
